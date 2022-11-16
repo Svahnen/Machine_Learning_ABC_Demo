1e9116ec81c7e4f13ed6c2b20ac442838d82ef18
BuildTools <unconfigured@null.spigotmc.org>
2022-11-16 11:43:04 +0100

    Add more test data

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from skimage.measure import find_contours
 from skimage.transform import resize
 
 # Ignores the warning about f1_score not working when some scores are 0
-warnings.filterwarnings('ignore')
+# warnings.filterwarnings('ignore')
 
 
 nr_of_letters = 26  # There are 26 english letters
@@ -51,7 +51,7 @@ def prepare_img(filename):  # Function to prepare images
 
 
 # Loop through and prepare all images
-srcdir = 'abc/'
+srcdir = 'A-Z/'
 np.set_printoptions(precision=2)
 
 abclist = []
@@ -75,15 +75,28 @@ y = df['abc']  # The abc column is the letter its supposed to be
 # Group by letter and calculate the mean of each column
 pix_mean = X.groupby(y).mean()
 
+"""
 fig, ax = plt.subplots(nr_of_letters)
 
 # show all the letters in a plot
 for i in range(nr_of_letters):
     ax[i].imshow(pix_mean.iloc[i, :].values.reshape(8, 8), cmap='gray')
     ax[i].set_title(i)
-    ax[i].set_yticks(range(8))
-    ax[i].set_xticks(range(8))
+    # ax[i].set_yticks(range(8))
+    # ax[i].set_xticks(range(8))
+    # hide the ticks
+    ax[i].set_yticklabels([])
+    ax[i].set_xticklabels([])
+"""
 
+fig, ax = plt.subplots(3, 8, figsize=(8, 3))
+for i in range(3):
+    for j in range(8):
+        ax[i, j].imshow(
+            pix_mean.iloc[i * 8 + j, :].values.reshape(8, 8), cmap='gray')
+        ax[i, j].set_title(i * 8 + j)
+        ax[i, j].set_yticklabels([])
+        ax[i, j].set_xticklabels([])
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, random_state=1)  # Split data into train and test
@@ -93,14 +106,34 @@ X_train, X_test, y_train, y_test = train_test_split(
 models = [DecisionTreeClassifier(), SVC(), SVC(kernel='linear'), KNeighborsClassifier(
 ), RandomForestClassifier(), GradientBoostingClassifier(), AdaBoostClassifier(), MLPClassifier()]
 
+
 # Loop through all models and print the accuracy
 for model in models:
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
+    # Print the letters that we will predict
     print("Model: ", model, "Classification report:\n",
           classification_report(y_test, y_pred))
     print("Model: ", model, "Confusion matrix:\n",
           confusion_matrix(y_test, y_pred))
     print("Model:", model, "Accuracy:", accuracy_score(y_test, y_pred))
+
+
+"""
+# Plot SVC Linear
+svc_linear = SVC(kernel='linear')
+svc_linear.fit(X_train, y_train)
+y_pred = svc_linear.predict(X_test)
+# show the confusion matrix with matplotlib
+fig, ax = plt.subplots()
+ax.matshow(confusion_matrix(y_test, y_pred), cmap=plt.cm.Blues)
+# Show corresponding letters on the x and y axis
+ax.set_xticklabels([''] + list(pix_mean.index))
+ax.set_yticklabels([''] + list(pix_mean.index))
+
+plt.xlabel('Predicted')
+plt.ylabel('True')
+
+"""
 
 plt.show()
